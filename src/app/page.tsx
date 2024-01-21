c306b23b6addec0ac35515d3d2b4be5d07e33714
VeronikaMelnik <vz2450wet.19@gmail.com>
2024-01-21 05:11:56 +0300

    changed icons in footer contacts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+'use client'
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.main}></div>;
+  const tmp = () => {
+    const user = {
+      name: 'John',
+      email: 'Johnsmith@mail.ru',
+      password: '12345678910',
+    }
+    fetch('http://localhost:5000/api/auth/signup/',
+      {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify(user)
+      }).then((value) => {
+        console.log(value)
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
+  return <div className={styles.main}>
+    <button onClick={tmp}>
+      Button
+    </button>
+  </div>;
 }
--- src/components/GlobalLayout/Footer/Contacts/Contacts.tsx
+++ src/components/GlobalLayout/Footer/Contacts/Contacts.tsx
@@ -1,26 +1,21 @@
 import styles from "./Contacts.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import {
-  faInstagram,
-  faYoutube,
-  faVk,
-} from "@fortawesome/free-brands-svg-icons";
-
-library.add(fas);
+import { Envelope } from "./Envelope/Envelope";
+import { Instagram } from "./Instagram/Instagram";
+import { Phone } from "./Phone/Phone";
+import { Vk } from "./Vk/Vk";
+import { Youtube } from "./Youtube/Youtube";
 
 export const Contacts = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.socials1}>
         <a href="tel:+375293267922" className={styles.a}>
-          <FontAwesomeIcon icon="phone" className={styles.icon} /> &nbsp;
-          +375(29)326-79-22
+          <Phone />
+          <p className={styles.text}>+375(29)326-79-22</p>
         </a>
         <a href="mailto:veronikamelnik00@mail.ru" className={styles.a}>
-          <FontAwesomeIcon icon="envelope" className={styles.icon} /> &nbsp;
-          cheerleader@tut.by
+          <Envelope />
+          <p className={styles.text}>cheerleader@tut.by</p>
         </a>
       </div>
       <div className={styles.socials2}>
@@ -30,17 +25,17 @@ export const Contacts = () => {
           rel="noreferrer"
           className={styles.a}
         >
-          <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+          <div className={styles.icon}><Instagram /></div>
         </a>
         <a
           href="https://www.youtube.com/@cheerleadinginbelarus4366"
           target="_blank"
           rel="noreferrer"
         >
-          <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
+          <div className={styles.icon}><Youtube /></div>
         </a>
         <a href="https://vk.com/bfchkp" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faVk} className={styles.icon} />
+          <div className={styles.icon}><Vk /></div>
         </a>
       </div>
     </div>
