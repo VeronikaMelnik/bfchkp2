42c360d6c6fa225141f07b7f8e9d0615b370ccf9
VeronikaMelnik <vz2450wet.19@gmail.com>
2024-03-28 00:29:11 +0300

    fixed registration page

--- src/_pages/registration/Registration.tsx
+++ src/_pages/registration/Registration.tsx
@@ -1,10 +1,8 @@
-"use client";
+"use server";
 
-import { useState } from "react";
+import { RegistrationInfo } from "widgets/layouts/global/RegistrationInfo";
 import { getRegistrationDictionary } from "features/server/localization/registration";
 import { DEFAULT_LANGUAGE, LanguageEnum } from "@entities/constants";
-import { Button, TextField } from "@entities/ui";
-import styles from "./Registration.module.scss";
 
 type Props = Readonly<{
   params: { lang: LanguageEnum };
@@ -13,72 +11,6 @@ type Props = Readonly<{
 export const Registration = async ({
   params: { lang = DEFAULT_LANGUAGE },
 }: Props) => {
-  const [login, setLogin] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [surname, setSurname] = useState<string>("");
-
-  const onSubmit = () => {
-    fetch("/api/form", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ login, password }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data: { token: string }) => {
-        window.localStorage.setItem("token", data.token);
-      })
-      .catch((error) => console.error("Error:", error));
-  };
-
   const dict = await getRegistrationDictionary(lang);
-  return (
-    <div className={styles.wrapper}>
-      <h1>{dict.registration}</h1>
-      <div className={styles.field}>
-        <label>{dict.name}</label>
-        <TextField
-          type="string"
-          value={name}
-          onChange={(ev) => {
-            setName(ev.target.value);
-          }}
-        />
-      </div>
-      <div className={styles.field}>
-        <label>{dict.surname}</label>
-        <TextField
-          type="string"
-          value={surname}
-          onChange={(ev) => {
-            setSurname(ev.target.value);
-          }}
-        />
-      </div>
-      <div className={styles.field}>
-        <label>{dict.login}</label>
-        <TextField
-          type="string"
-          value={login}
-          onChange={(ev) => {
-            setLogin(ev.target.value);
-          }}
-        />
-      </div>
-      <div className={styles.field}>
-        <label>{dict.password}</label>
-        <TextField
-          type="string"
-          onChange={(ev) => {
-            setPassword(ev.target.value);
-          }}
-        />
-      </div>
-      <Button onClick={onSubmit}>{dict.button}</Button>
-    </div>
-  );
+  return <RegistrationInfo dict={dict} />;
 };
--- src/widgets/layouts/global/RegistrationInfo/RegistrationInfo.tsx
+++ src/widgets/layouts/global/RegistrationInfo/RegistrationInfo.tsx
@@ -1,10 +1,79 @@
-import classNames from "classnames";
+"use client";
+
+import { useState } from "react";
+import { Button, TextField } from "@entities/ui";
 import styles from "./RegistrationInfo.module.scss";
 
-interface Props {
-  className?: string;
-}
+type Props = Readonly<{
+  dict: any;
+}>;
 
-export const RegistrationInfo = ({ className }: Props) => {
-  return <div className={classNames(styles.wrapper, className)}></div>;
+export const RegistrationInfo = ({ dict }: Props) => {
+  const [name, setName] = useState<string>("");
+  const [surname, setSurname] = useState<string>("");
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const onSubmit = () => {
+    fetch("/api/form", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, surname, login, password }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data: { token: string }) => {
+        window.localStorage.setItem("token", data.token);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>{dict.registration}</h1>
+      <div className={styles.field}>
+        <label>{dict.name}</label>
+        <TextField
+          type="string"
+          value={name}
+          onChange={(ev) => {
+            setName(ev.target.value);
+          }}
+        />
+      </div>
+      <div className={styles.field}>
+        <label>{dict.surname}</label>
+        <TextField
+          type="string"
+          value={surname}
+          onChange={(ev) => {
+            setSurname(ev.target.value);
+          }}
+        />
+      </div>
+      <div className={styles.field}>
+        <label>{dict.login}</label>
+        <TextField
+          type="string"
+          value={login}
+          onChange={(ev) => {
+            setLogin(ev.target.value);
+          }}
+        />
+      </div>
+      <div className={styles.field}>
+        <label>{dict.password}</label>
+        <TextField
+          type="string"
+          onChange={(ev) => {
+            setPassword(ev.target.value);
+          }}
+        />
+      </div>
+      <Button onClick={onSubmit}>{dict.button}</Button>
+    </div>
+  );
 };
